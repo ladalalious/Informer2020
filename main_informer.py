123d006a93b47c0826a871095b6911c4363c24aa
yujie <951280040@qq.com>
2023-12-16 21:35:34 +0800

    变更

--- main_informer.py
+++ main_informer.py
@@ -4,27 +4,40 @@ import torch
 
 from exp.exp_informer import Exp_Informer
 
+#创建了一个 ArgumentParser 对象，它用于解析命令行参数
 parser = argparse.ArgumentParser(description='[Informer] Long Sequences Forecasting')
 
+#使用什么网络结构
 parser.add_argument('--model', type=str, required=True, default='informer',help='model of experiment, options: [informer, informerstack, informerlight(TBD)]')
-
+#读取哪个数据集
 parser.add_argument('--data', type=str, required=True, default='ETTh1', help='data')
 parser.add_argument('--root_path', type=str, default='./data/ETT/', help='root path of the data file')
-parser.add_argument('--data_path', type=str, default='ETTh1.csv', help='data file')    
+parser.add_argument('--data_path', type=str, default='ETTh1.csv', help='data file')
+#特征参数，M表示多变量预测多变量，S表示单变量预测单变量，MS表示多变量预测单变量
 parser.add_argument('--features', type=str, default='M', help='forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, S:univariate predict univariate, MS:multivariate predict univariate')
+#数据标签
 parser.add_argument('--target', type=str, default='OT', help='target feature in S or MS task')
+#用于时间特征编码的频率
 parser.add_argument('--freq', type=str, default='h', help='freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h')
+#在深度学习中，特别是在模型训练过程中，"checkpoints" 是指在训练过程中定期保存模型的状态，包括模型的权重、优化器的状态以及其他必要的信息。这样做的目的是为了在训练过程中发生意外中断（例如计算机崩溃、断电等）时，能够从最后一个保存的 checkpoint 处继续训练，而不需要重新开始整个训练过程。
 parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
-
+#输入时序数据的长度
 parser.add_argument('--seq_len', type=int, default=96, help='input sequence length of Informer encoder')
+#作为预测先验知识的时序数据的长度
 parser.add_argument('--label_len', type=int, default=48, help='start token length of Informer decoder')
+#预测的长度
 parser.add_argument('--pred_len', type=int, default=24, help='prediction sequence length')
 # Informer decoder input: concat[start token series(label_len), zero padding series(pred_len)]
+# 在 Informer 模型的解码器（decoder）中，关于输入的说明是指在解码器的输入中，首先包含了时间序列中的标签部分（label_len），然后是零填充序列（zero padding series）用于表示预测的部分（pred_len）。
 
+#encoder输入的维度
 parser.add_argument('--enc_in', type=int, default=7, help='encoder input size')
 parser.add_argument('--dec_in', type=int, default=7, help='decoder input size')
+#输出的维度
 parser.add_argument('--c_out', type=int, default=7, help='output size')
+#隐藏层的维度
 parser.add_argument('--d_model', type=int, default=512, help='dimension of model')
+#注意力头数
 parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
 parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
 parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
@@ -32,9 +45,11 @@ parser.add_argument('--s_layers', type=str, default='3,2,1', help='num of stack 
 parser.add_argument('--d_ff', type=int, default=2048, help='dimension of fcn')
 parser.add_argument('--factor', type=int, default=5, help='probsparse attn factor')
 parser.add_argument('--padding', type=int, default=0, help='padding type')
+#是否启用蒸馏
 parser.add_argument('--distil', action='store_false', help='whether to use distilling in encoder, using this argument means not using distilling', default=True)
 parser.add_argument('--dropout', type=float, default=0.05, help='dropout')
 parser.add_argument('--attn', type=str, default='prob', help='attention used in encoder, options:[prob, full]')
+#编码方式
 parser.add_argument('--embed', type=str, default='timeF', help='time features encoding, options:[timeF, fixed, learned]')
 parser.add_argument('--activation', type=str, default='gelu',help='activation')
 parser.add_argument('--output_attention', action='store_true', help='whether to output attention in ecoder')
@@ -58,6 +73,7 @@ parser.add_argument('--gpu', type=int, default=0, help='gpu')
 parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
 parser.add_argument('--devices', type=str, default='0,1,2,3',help='device ids of multile gpus')
 
+#parse_args 方法会解析命令行参数，并返回一个包含这些参数值的对象（通常是命名空间对象）。在你的代码中，这个对象被赋值给了 args 变量。
 args = parser.parse_args()
 
 args.use_gpu = True if torch.cuda.is_available() and args.use_gpu else False
