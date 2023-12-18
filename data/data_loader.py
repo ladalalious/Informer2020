561bc0281574448e5ee9c2903a6d154f92343849
mmzj <mmzj@163.com>
2023-12-18 15:39:47 +0800

    对预测数据集进行了修改

--- data/data_loader.py
+++ data/data_loader.py
@@ -371,7 +371,9 @@ class Dataset_Pred(Dataset):
         seq_x_mark = self.data_stamp[s_begin:s_end]
         seq_y_mark = self.data_stamp[r_begin:r_end]
 
-        return seq_x, seq_y, seq_x_mark, seq_y_mark
+        # return seq_x, seq_y, seq_x_mark, seq_y_mark
+
+        return seq_x, seq_y
     
     def __len__(self):
         return len(self.data_x) - self.seq_len + 1
